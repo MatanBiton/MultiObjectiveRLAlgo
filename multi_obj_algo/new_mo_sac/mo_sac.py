6bd2d22b57d770575643735cae944395772f16e6
MatanBiton <matanb1905@gmail.com>
2025-06-27 12:30:39 +0300

    f

--- multi_obj_algo/new_mo_sac/mo_sac.py
+++ multi_obj_algo/new_mo_sac/mo_sac.py
@@ -190,7 +190,7 @@ class MOSAC:
             obs, _ = self.env.reset()
             episode_rewards = np.zeros(self.objectives)
             last_info = None
-            for t in range(self.max_steps):
+            for _ in range(self.max_steps):
                 action = self.select_action(obs)
                 next_obs, reward, terminated, truncated, info = self.env.step(action)
                 last_info = info
@@ -198,21 +198,23 @@ class MOSAC:
                 episode_rewards += reward
 
                 # Raw per-step action logging
-                if self.verbose:
+                if self.verbose and 'iso' in info and 'pcs' in info:
                     iso_actions = info['iso'].get('actions', [])
-                    if len(iso_actions) > t:
-                        act = iso_actions[t]
+                    if len(iso_actions) > self.global_step:
+                        act = iso_actions[self.global_step]
                         for comp, val in enumerate(act):
                             self.writer.add_scalar(
-                                f'Raw/iso/action_comp{comp}', val, t
+                                f'Raw/iso/action_comp{comp}', val, self.global_step
                             )
                     pcs_actions = info['pcs'].get('actions', [])
-                    if len(pcs_actions) > t:
-                        act_pcs = pcs_actions[t]
+                    if len(pcs_actions) > self.global_step:
+                        act_pcs = pcs_actions[self.global_step]
                         for comp, val in enumerate(act_pcs):
                             self.writer.add_scalar(
-                                f'Raw/pcs/action_comp{comp}', val, t
+                                f'Raw/pcs/action_comp{comp}', val, self.global_step
                             )
+                    self.global_step += 1
+                    self.writer.add_scalar(f"global_step", self.global_step, self.global_step)
 
                 obs = next_obs
 
